e9ef628568d756d4da4084e695763c0ef9fa49d6
rokezo <dobusnikita@gmail.com>
2024-05-24 16:15:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int M, N;
+    cout << "entter elements A: ";
+    cin >> M;
+    cout << "enter elenments B: ";
+    cin >> N;
+
+    int* A = new int[M];
+    int* B = new int[N];
+
+    cout << "elements massiv  A: ";
+    for (int i = 0; i < M; i++) {
+        cin >> A[i];
+    }
+
+    cout << "elememnti massi vva B: ";
+    for (int i = 0; i < N; i++) {
+        cin >> B[i];
+    }
+
+    bool* existsInA = new bool[1000];
+    bool* existsInB = new bool[1000]; 
+    for (int i = 0; i < 1000; i++) {
+        existsInA[i] = false;
+        existsInB[i] = false;
+    }
+
+    for (int i = 0; i < M; i++) {
+        existsInA[A[i]] = true;
+    }
+
+    for (int i = 0; i < N; i++) {
+        existsInB[B[i]] = true;
+    }
+
+    int count = 0;
+    for (int i = 0; i < 1000; i++) {
+        if ((existsInA[i] && !existsInB[i]) || (!existsInA[i] && existsInB[i])) {
+            count++;
+        }
+    }
+
+    int* C = new int[count];
+    int j = 0;
+    for (int i = 0; i < 1000; i++) {
+        if ((existsInA[i] && !existsInB[i]) || (!existsInA[i] && existsInB[i])) {
+            C[j++] = i;
+        }
+    }
+
+    cout << "masiv  C: ";
+    for (int i = 0; i < count; i++) {
+        cout << C[i] << " ";
+    }
+    cout << endl;
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,62 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int M, N;
+    cout << "Введите размер массива A: ";
+    cin >> M;
+    cout << "Введите размер массива B: ";
+    cin >> N;
+
+    int* A = new int[M];
+    int* B = new int[N];
+
+    cout << "Введите элементы массива A: ";
+    for (int i = 0; i < M; i++) {
+        cin >> A[i];
+    }
+
+    cout << "Введите элементы массива B: ";
+    for (int i = 0; i < N; i++) {
+        cin >> B[i];
+    }
+
+    bool* existsInB = new bool[1000]; 
+    for (int i = 0; i < 1000; i++) {
+        existsInB[i] = false;
+    }
+
+    for (int i = 0; i < N; i++) {
+        existsInB[B[i]] = true;
+    }
+
+    int count = 0;
+    for (int i = 0; i < M; i++) {
+        if (!existsInB[A[i]]) {
+            count++;
+        }
+    }
+
+    int* C = new int[count];
+    int j = 0;
+    for (int i = 0; i < M; i++) {
+        if (!existsInB[A[i]]) {
+            C[j++] = A[i];
+        }
+    }
+
+    cout << "Массив C: ";
+    for (int i = 0; i < count; i++) {
+        cout << C[i] << " ";
+    }
+    cout << endl;
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
+    delete[] existsInB;
+
+    return 0;
+}
